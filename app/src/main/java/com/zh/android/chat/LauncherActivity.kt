c0c8064e87b459f8d01a93d21cc7f748663ac6b7
zihe <he100zihao100@126.com>
2020-10-28 15:32:39 +0800

    update: 限定主页面和启动页不允许侧滑返回

--- app/src/main/java/com/zh/android/chat/LauncherActivity.kt
+++ app/src/main/java/com/zh/android/chat/LauncherActivity.kt
@@ -4,6 +4,7 @@ import android.view.View
 import com.alibaba.android.arouter.facade.Postcard
 import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.callback.NavCallback
+import com.youngfeng.snake.annotations.EnableDragToClose
 import com.zh.android.base.constant.ARouterUrl
 import com.zh.android.base.core.BaseActivity
 import com.zh.android.chat.service.module.home.HomeService
@@ -13,6 +14,7 @@ import com.zh.android.chat.service.module.login.LoginService
  * @author wally
  * @date 2020/08/26
  */
+@EnableDragToClose(value = false)
 class LauncherActivity : BaseActivity() {
     @JvmField
     @Autowired(name = ARouterUrl.LOGIN_SERVICE)
